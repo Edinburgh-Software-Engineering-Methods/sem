edcc41ed33d99a33ef85a13171f7155380cc0b45
seinlei <seimleihtarni@gmail.com>
2024-04-03 13:28:30 +0630

    Added to display countries by continent
    Signed-off-by: seinlei <seimleihtarni@gmail.com>

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 
 public class App {
 
+    // main method for displaying countries list
     public static void main(String[] args) {
         // Create new Application
         App a = new App();
@@ -18,6 +19,10 @@ public class App {
         // Display country information
         a.displayCountry(countriesByPopulation);
 
+        ArrayList<Country> countriesByContinent = a.getCountriesByContinent("Asia");
+        System.out.println("All the countries in Asia by population: ");
+        a.displayCountry(countriesByContinent);
+
         ArrayList<Country> countriesByRegion = a.getCountriesByRegion("North America");
         System.out.println("All the countries in North America by population: ");
         a.displayCountry(countriesByRegion);
@@ -28,6 +33,7 @@ public class App {
 
     private Connection con = null;
 
+    // connection to database
     public void connect() {
         try {
             // Load Database driver
@@ -46,7 +52,7 @@ public class App {
 
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false",
-                        "root", "example"); // Change the database name to "world"
+                        "root", "example");
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
@@ -57,6 +63,8 @@ public class App {
             }
         }
     }
+
+    //get all the countries in the world by population
 
     public ArrayList<Country> getCountriesByPopulation() {
         try {
@@ -89,6 +97,33 @@ public class App {
         }
     }
 
+    // get all the countries in a continent ordered by population
+    public ArrayList<Country> getCountriesByContinent(String continent) {
+        try {
+            Statement stmt = con.createStatement();
+            String strSelect = "SELECT Code, Name, Continent, Region, Population, Capital " +
+                    "FROM country " + "WHERE Continent = '" + continent + "' " + "ORDER BY Population DESC";
+            ResultSet rset = stmt.executeQuery(strSelect);
+            ArrayList<Country> countries = new ArrayList<>();
+            while (rset.next()) {
+                Country ctry = new Country();
+                ctry.code = rset.getString("Code");
+                ctry.name = rset.getString("Name");
+                ctry.continent = rset.getString("Continent");
+                ctry.region = rset.getString("Region");
+                ctry.population = rset.getInt("Population");
+                ctry.capital = rset.getString("Capital");
+                countries.add(ctry);
+            }
+            return countries;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get countries by region");
+            return null;
+        }
+    }
+
+    // get all the countries in a region ordered by population
     public ArrayList<Country> getCountriesByRegion(String region) {
         try {
             Statement stmt = con.createStatement();
@@ -116,6 +151,7 @@ public class App {
 
     /**
      * Prints a list of countries.
+     *
      * @param country The country to print.
      */
     public void displayCountry(ArrayList<Country> country) {
@@ -125,6 +161,8 @@ public class App {
             System.out.println(ctry_String);
         }
     }
+
+    // disconnect from database
     public void disconnect() {
         if (con != null) {
             try {
