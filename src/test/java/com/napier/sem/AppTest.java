97975e9c5facf9f813376c4ef499535b2e2ac6f7
seinlei <seimleihtarni@gmail.com>
2024-04-21 15:54:14 +0630

    Updated App tests and Display top Capitals
    Signed-off-by: seinlei <seimleihtarni@gmail.com>

--- src/test/java/com/napier/sem/AppTest.java
+++ src/test/java/com/napier/sem/AppTest.java
@@ -1,43 +1,9 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Field;
-import java.sql.Connection;
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
-
 public class AppTest {
-
-    private App app;
-
-    @BeforeEach
-    public void setUp() {
-        app = new App();
-    }
-
-    @Test
-    public void testConnect() {
-        try {
-            app.connect("localhost:33060", 10000);
-        } catch (Exception e) {
-            fail("Exception thrown while connecting to the database: " + e.getMessage());
-        }
-
-        // Use reflection to access the con field
-        try {
-            Field conField = App.class.getDeclaredField("con");
-            conField.setAccessible(true);
-            Connection connection = (Connection) conField.get(app);
-            assertNotNull(connection, "Connection should not be null after successful connection");
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            fail("Failed to access the con field in the App class");
-        }
-    }
 
     @Test
     public void testDisplayCity() {
@@ -47,7 +13,9 @@ public class AppTest {
         cities.add(new City("Los Angeles", "United States", "California", 4000000));
 
         // Call the method
+        App app = new App();
         app.displayCity(cities);
+
     }
 
     @Test
@@ -58,7 +26,9 @@ public class AppTest {
         countries.add(new Country("United States", 1000000, "Washington"));
 
         // Call the method
+        App app = new App();
         app.displayCapitalCity(countries);
+
     }
 
     @Test
@@ -69,11 +39,8 @@ public class AppTest {
         countries.add(new Country("CAN", "Canada", "Europe", "North America", 2000000, "Ottawa"));
 
         // Call the method
+        App app = new App();
         app.displayCountry(countries);
-    }
 
-    @AfterEach
-    public void tearDown() {
-        app.disconnect();
     }
 }
